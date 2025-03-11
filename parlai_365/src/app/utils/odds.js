d56bb30c6b2d4bc8333ae29eb19b56fe3adff42a
Pahul Brar <pahul.brar@gettyimages.com>
2025-03-11 00:07:09 -0600

    Need to figure out how to get gemni api working

--- parlai_365/src/app/utils/odds.js
+++ parlai_365/src/app/utils/odds.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 const axios = require('axios');
 
-// This is your reusable component
 const OddsFetcher = ({ sportKey, regions, markets, oddsFormat, dateFormat }) => {
   // State to store the data, loading state, and error
 //   const [data, setData] = useState(null);
@@ -41,30 +40,7 @@ const [sportsData, setSportsData] = useState(null);
         setLoading(false);
       });
   }, [sportKey, regions, markets, oddsFormat, dateFormat]);
-//   useEffect(() => {
-//     // Construct the URL to fetch odds data
-//     const url = `https://api.the-odds-api.com/v4/sports/${sportKey}/odds`;
 
-//     // Make the API call using axios
-//     axios
-//       .get(url, {
-//         params: {
-//           apiKey,
-//           regions,
-//           markets,
-//           oddsFormat,
-//           dateFormat,
-//         },
-//       })
-//       .then((response) => {
-//         setData(response.data);  // Save the response data to state
-//         setLoading(false);        // Data fetched, stop loading
-//       })
-//       .catch((error) => {
-//         setError(error.message);  // If error occurs, save the error message
-//         setLoading(false);        // Stop loading even if there was an error
-//       });
-//   }, [sportKey, regions, markets, oddsFormat, dateFormat]); // Dependency array
 
   // Show loading indicator
   if (loading) return <div>Loading...</div>;
